8d33f7e273935734a4e1a68dd26851656bf0b998
satyambalaiwar <118449341+satyambalaiwar@users.noreply.github.com>
2024-10-28 15:29:36 +0530

    bloggingsite vite

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Button>Click</Button>
+      
+    </div>
+  )
+}
+
+export default App
